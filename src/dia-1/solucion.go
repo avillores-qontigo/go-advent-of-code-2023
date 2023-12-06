79159a11550cc676bbd0b7405bef81f09aca2a91
Alejo Villores <avillores@qontigo.com>
2023-12-06 12:55:47 -0300

    add: pre solution

--- src/dia-1/solucion.go
+++ src/dia-1/solucion.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"regexp"
+)
+
+func removeNonNumeric(input string) string {
+	// Use a regular expression to match only numeric characters
+	numericRegex := regexp.MustCompile("[^0-9]")
+	result := numericRegex.ReplaceAllString(input, "")
+
+	return result
+}
 
 func main() {
 	fmt.Println("Hello world !!")
+
+	// abro el archivo
+
+	// por cada linea tiro una task
+
+	// voy sumando por channel las sumas parciales
+
+	// cierro el channel y devuelvo el resultado por pantalla
 }
