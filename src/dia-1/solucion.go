14cf22abf9255ccd0cc779abf2321e84d02917a6
Alejo Villores <avillores@qontigo.com>
2023-12-07 10:36:50 -0300

    add: iterative solution

--- src/dia-1/solucion.go
+++ src/dia-1/solucion.go
@@ -1,26 +1,57 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"regexp"
+	"strconv"
 )
 
-func removeNonNumeric(input string) string {
+func removeNonNumeric(line string) string {
 	// Use a regular expression to match only numeric characters
-	numericRegex := regexp.MustCompile("[^0-9]")
-	result := numericRegex.ReplaceAllString(input, "")
+	numericRegex := regexp.MustCompile("[^-?\\d+$]")
+	result := numericRegex.ReplaceAllString(line, "")
 
 	return result
 }
 
+func processLine(line string) int {
+	lineWithoutLetters := removeNonNumeric(line)
+	total := 0
+	for _, number := range lineWithoutLetters {
+		num, err := strconv.Atoi(string(number))
+		if err != nil {
+			fmt.Println("err:", err)
+		}
+		total += num
+	}
+	return total
+}
+
 func main() {
-	fmt.Println("Hello world !!")
+	total := 0
 
 	// abro el archivo
+	file, err := os.Open("src/dia-1/input.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+	// Creo el channel por donde mando los resultados
 
-	// por cada linea tiro una task
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		// por cada linea tiro una task
+		line := scanner.Text()
+		fmt.Println(line)
 
-	// voy sumando por channel las sumas parciales
+		total += processLine(line)
+	}
 
-	// cierro el channel y devuelvo el resultado por pantalla
+	if err := scanner.Err(); err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Result:", total)
+
 }
